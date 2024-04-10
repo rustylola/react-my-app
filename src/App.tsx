06e4bad82e52451f524fe6b0ceb6f625327200cc
lola <johnrustylola@gmail.com>
2024-04-11 00:41:01 +0800

    adding todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
 
 
 // F.C is Functional Component
 // Type React. to view all types of it
 const App:React.FC = () => {
 
+  // State wherein 
   const [todo,setTodo] = useState<string>("");
-  
+  const [todos,setTodos] = useState<Todo[]>([]);
+
+  const handleAdd=(e:React.FormEvent)=>{
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo:todo,isDone:false}])
+      setTodo("");
+    }
+  };
+
+  // console.log(todos)
+
   return (
     <div className="App">
       <span className="heading">TaskTify</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./style.css";
 
 interface Props{
     todo:string;
     setTodo:React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:(e:React.FormEvent)=> void;
 }
-const InputField: React.FC<Props> = ({todo,setTodo}) => {
-  return (
-    <form className="input">
+const InputField: React.FC<Props> = ({todo,setTodo,handleAdd}) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+  
+    return (
+    <form className="input" onSubmit={(e) => {
+            handleAdd(e);
+            inputRef.current?.blur();
+            // inputRef.current?.blur();
+        }}>
       <input
+        ref={inputRef}
         type="input"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
